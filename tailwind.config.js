c4a65bd197b6ec684c793a23f7f345dc44a204ca
rshiva01 <shivaraman.vns03@gmail.com>
2024-02-06 19:39:52 +0530

    login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,40 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'login-page-background':"url('/images/loginBgImage.png')"
+      },
+      screens: {
+        'sm': {'max':'930px'},
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      fontFamily:{
+        poppins: ['Poppins', 'sans-serif']
+      }
+    },
+    colors:{
+      'purple':'#190019',
+      'white':'#FBE4D8',
+      'pink':'#DFB6B2',
+      'dark-pink':"#854F6C",
+      'pure-white':"#FFFFFF"
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
 
