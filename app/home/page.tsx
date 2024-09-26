ddd5cd5b94272f4486c419f3dc7365c561445d60
rshiva01 <shivaraman.vns03@gmail.com>
2024-09-26 18:21:25 +0530

    connected with the backend and also made component for the vendor

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,23 +1,40 @@
-import { HotelSharp, Logout, Person, ShoppingBag } from '@mui/icons-material';
+"use client"
+
 import Vendor from './components/vendors';
-import Link from 'next/link';
 import Header from '../components/header';
+import { useEffect, useState } from 'react';
+
+async function fetchVendors() {
+  const response = await fetch('http://localhost:8000/vendor');
+  const data = await response.json();
+  return data;
+}
+
+function useVendors() {
+  const [vendors, setVendors] = useState([]);
+
+  useEffect(() => {
+    fetchVendors().then((data) => {
+      setVendors(data);
+    });
+  }, []);
+
+  return vendors;
+}
+
+
 
 export default function Home() {
-  const vendors = [
-    { name: 'PESU Food Court', imagesrc: './images/foodImage.png' },
-    { name: 'Halli Mane', imagesrc: './images/foodImage.png' },
-    { name: 'Cantina', imagesrc: './images/foodImage.png' },
-  ];
+  const vendors = useVendors();
   return (
     <main className='h-screen bg-purple'>
       <Header/>
       <div className='mx-[5vw] w-[90vw] overscroll-none sm:mt-[5vh] md:mt-[10vh]'>
         <ul className='justify-between md:flex'>
-          {vendors.map((x) => (
-            <div>
-              <Vendor key={x.name} name={x.name} imagesrc={x.imagesrc} />
-            </div>
+          {vendors.map((vendor) => (
+            <li key={vendor.vendor_id}>
+              <Vendor name={vendor.vendor_name} imagesrc={"../images/foodImage.png"} />
+            </li>
           ))}
         </ul>
       </div>
